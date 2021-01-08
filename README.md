565caee08b6621810ebbc9eff09e54f7a742c4b7
Yang Zhang <zy910304@gmail.com>
2021-01-08 23:36:02 +0100

    revision readme

--- README.md
+++ README.md
@@ -1,35 +1,88 @@
 # Coffee Shop Full Stack
 
-## Full Stack Nano - IAM Final Project
+## Introduction
 
 Udacity has decided to open a new digitally enabled cafe for students to order drinks, socialize, and study hard. But they need help setting up their menu experience.
 
-You have been called on to demonstrate your newly learned skills to create a full stack drink menu application. The application must:
+ The application is capable of:
 
 1) Display graphics representing the ratios of ingredients in each drink.
 2) Allow public users to view drink names and graphics.
 3) Allow the shop baristas to see the recipe information.
 4) Allow the shop managers to create new drinks and edit existing drinks.
 
-## Tasks
+## Table of contents
+  - [Introduction](#Introduction)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview))
+  - [Project Structure](#project-structure)
+  - [How to use the application](#how-to-use-the-application)
+  - [Authors](#authors)
 
-There are `@TODO` comments throughout the project. We recommend tackling the sections in order. Start by reading the READMEs in:
+## Overview
+### Backend Dependencies
+ * **virtualenv** as a tool to create isolated Python environments
+ * **SQLAlchemy ORM** to be our ORM library of choice
+ * **PostgreSQL** as our database of choice
+ * **Python3** and **Flask** as our server language and server framework
+ * **Flask-CORS** the extension to handle cross origin requests from our frontend server. <br>
 
-1. [`./backend/`](./backend/README.md)
-2. [`./frontend/`](./frontend/README.md)
+More detailed information can be found in the  [readme file](./backend/README.md) in the backend folder. 
+### Frontend Dependencies
+This project uses NPM to manage software dependencies. NPM Relies on the package.json file located in the `frontend` directory of this repository. <br>
+`ionic` must be installed. Detailed method of installing can be found at [here](https://ionicframework.com/docs/intro/cli). There is a issue of executing `ionic serve` with windows environment, please refer to this [stack overflow page](https://stackoverflow.com/questions/29331276/ionic-framework-ionic-is-not-recognized-as-an-internal-or-external-command). <br>
+More detailed information can be found in the  [readme file](./frontend/README.md) in the frontend folder. 
 
-## About the Stack
+## Project Structure
+```sh
+  ├── README.md
+  ├── backend
+      ├── src -- the main application
+          ├──auth: foler for authentication and permission control
+          ├──database: folder for database
+          ├──api.py: main application
+          ├──__init__.py
+      ├── README.md -- readme file for backend.
+      ├── requirements.txt -- The dependencies to run the backend
+      ├── udacity-fsnd-udaspicelatte.postman_collection.json -- Use [postmen](https://web.postman.co/home) to test the API
+  ├── frontend
+      ├── e2e
+      ├── src
+          ├── environments -- folder contain setups that define the interactions between API and backend
+          ├── ...
+      ├── packages.json -- dependencies to run the frontend
+      ├── ...
+```
+Overall:
+* The backend application and databases are in the `backend` folder, where we mainly define the APIs. APIs are further called in the front end. 
+* The `frontend` directory contains a complete React frontend to consume the data from the Flask server. The frontend uses the APIs provided by the backend to have interactions with the application user.  
 
-We started the full stack application for you. It is desiged with some key functional areas:
+## How to use the application
+1. Understand the Project Structure (explained above) and where important files are located.
+2. git clone this repo to your local folder using `https://github.com/hellogaga/udacity_coffee.git`
+3. Install all the dependencies (both frontend and backend) according to the instructions. 
+4. Navigate to **backend/src**, start the backend using the following commands (linux environment):
+```bash
+export FLASK_APP=api.py
+export FLASK_ENV=development
+flask run
+```
+5. Open a **new** bash console. Navigate to folder `frontend` and run the following commands. They will start the front end. 
+```bash
+npm install \*only need to execute once*\
+npm npm install -g @ionic/cli \*only need to execute once*\
+ionic serve
+```
+6. Navigate to project homepage [http://127.0.0.1:8100/](http://127.0.0.1:8100/) or [http://localhost:8100](http://localhost:8100) 
+7. Enjoy the application.
 
-### Backend
+### Test the backend
+To test the backend:
+1. start the backend
+2. open [postmen](https://web.postman.co/home)
+3. import the `backend\udacity-fsnd-udaspicelatte.postman_collection.json`
+4. replace the tokens in `barista` and `manager` with valid ones
+5. run the test
 
-The `./backend` directory contains a partially completed Flask server with a pre-written SQLAlchemy module to simplify your data needs. You will need to complete the required endpoints, configure, and integrate Auth0 for authentication.
-
-[View the README.md within ./backend for more details.](./backend/README.md)
-
-### Frontend
-
-The `./frontend` directory contains a complete Ionic frontend to consume the data from the Flask server. You will only need to update the environment variables found within (./frontend/src/environment/environment.ts) to reflect the Auth0 configuration details set up for the backend app. 
-
-[View the README.md within ./frontend for more details.](./frontend/README.md)
+## Authors
+* Hellogaga used the starting code from [udacity](www.udacity.com) to finish the API and test code.
--- backend/src/api.py
+++ backend/src/api.py
@@ -14,9 +14,7 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app,resources={r"/*":{"origins":'*'}})
 
-'''
-Not clear if the following is necessary
-'''
+# CORS setup
 @app.after_request
 def after_request(response):
   #response.
@@ -25,14 +23,12 @@ def after_request(response):
   return response
 
 '''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
 db_drop_and_create_all()
 
 # Because there is no drink in the database,
-# Add a mock beverage, otherwise tests cannot be passed
+# Add a mock beverage
 recipe= [{
   "name": "Cola",
   "color": "Black",
@@ -50,12 +46,19 @@ new_drink.insert()
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/')
+def index_page():
+  return ('Coffee shop powered by Hellogaga')
+
 @app.route('/drinks')
 def coffee():
+  '''
+  endpoint for get all drinks
+  No permission required
+  '''
   # get all drinks
   drinks = Drink.query.all()
   
-  print (drinks)
   # abort if no data 
   if len(drinks) == 0:
     abort(404)
@@ -72,21 +75,17 @@ def coffee():
   }),200
 
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
 def get_drink_detail(jwt_token):
+  '''
+  Endpoint for get all drink details. 
+  Permission is checked.
+  '''
+
   # get all drinks
   drinks = Drink.query.all()
   
-  print (drinks)
   # abort if no data 
   if len(drinks) == 0:
     abort(404)
@@ -102,18 +101,15 @@ def get_drink_detail(jwt_token):
     'drinks':drinks_list
   }),200
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def post_a_new_drink(jwt_token):
+  '''
+  Endpoint for create a new drink. 
+  Permission is checked.
+  '''
+  # check input data
   data = request.get_json()
   if 'title' and 'recipe' not in data:
     abort(422)
@@ -123,38 +119,34 @@ def post_a_new_drink(jwt_token):
   recipe_json = json.dumps(data['recipe'])
   drink = Drink(title=title, recipe=recipe_json)
   drink.insert()
-
+  
+  # return message
   return jsonify({
       'success': True,
       'drinks': [drink.long()]
   })
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks/<int:id>', methods=['PATCH'])
 @requires_auth('post:drinks')
 def patch_a_drink(jwt_token,id):
+  '''
+  Endpoint for revise a new drink. 
+  Permission is checked.
+  '''
+  # check if drink exists
   drink = Drink.query.get(id)
   if drink is None:
       abort(404)
-
+  
+  # check the input data
   data = request.get_json()
   if 'title' in data:
       drink.title = data['title']
-
   if 'recipe' in data:
       drink.recipe = json.dumps(data['recipe'])
-
+  
+  # update the drink
   drink.update()
 
   return jsonify({
@@ -163,26 +155,22 @@ def patch_a_drink(jwt_token,id):
   })
 
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-
 @app.route('/drinks/<int:id>', methods=['DELETE'])
 @requires_auth('post:drinks')
 def delete_a_drink(jwt_token,id):
+  '''
+  Endpoint for delete a drink. 
+  Permission is checked.
+  '''
+  # check if drink exists
   drink = Drink.query.get(id)
   if drink is None:
       abort(404)
 
+  # if exists, delete
   drink.delete()
-
+  
+  # return message
   return jsonify({
       'success': True,
       'delete': drink.id
@@ -190,7 +178,7 @@ def delete_a_drink(jwt_token,id):
 
 ## Error Handling
 '''
-Example error handling for unprocessable entity
+error handling for 422 404 400 403 500 401
 '''
 @app.errorhandler(422)
 def unprocessable(error):
@@ -200,16 +188,15 @@ def unprocessable(error):
     "message": "unprocessable"
     }), 422
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
+@app.errorhandler(401)
+def unauthorized(error):
+  return jsonify({
+    "success": False, 
+    "error": 401,
+    "message": "unauthorized"
+    }), 401
 
-'''
+
 @app.errorhandler(404)
 def not_found(error):
   return jsonify({
